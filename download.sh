40d28f0d3896df5d8a120946c2dcb1c83db2af83
NHNkimjaemin <jaemin.kim@nhn.com>
2024-04-08 11:09:27 +0900

    빌드 후 커밋

--- download.sh
+++ download.sh
@@ -5,7 +5,8 @@ cd $HOME/Projects/lunch-bot/
 # Get the current date in the desired format
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 pnpm run download
+pnpm run build
 
-git add lunch.json
+git add lunch.json dist/
 git commit -m "Commit-$DATE"
 git push

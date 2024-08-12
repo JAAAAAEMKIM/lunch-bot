a6e38abbc2772f72a2ffbf9e8ec4772be1511b9e
NHNKimjaemin <jaemin.kim@nhn.com>
2024-08-12 10:50:17 +0900

    Commit-2024-08-12 10:50:03

--- dist/src/constants.d.ts
+++ dist/src/constants.d.ts
@@ -1,9 +1,10 @@
 export declare const COURSE_REGEX: RegExp;
 export declare const PLUS_REGEX: RegExp;
 export declare const CONCEPT_REGEX: RegExp;
-export declare const COLUMN_COUNT = 4;
-export declare const CALORY_IDX = 3;
+export declare const COLUMN_COUNT = 6;
+export declare const CALORY_IDX = 5;
 export declare const RATIO_IDX = 2;
+export declare const PROTEIN_IDX = 4;
 export declare const BOT_API_URL: {
     FE: string;
     GROUP: string;

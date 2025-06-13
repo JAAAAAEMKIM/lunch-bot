5372354dc680323e8fe921fcb811a000c1be01a6
NHNKimjaemin <jaemin.kim@nhn.com>
2025-06-13 20:58:36 +0900

    버전 18로 변경 (Blob 대응)

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NODE_BIN_PATH=/home1/irteam/.nvm/versions/node/v16.13.1/bin
+NODE_BIN_PATH=/home1/irteam/.nvm/versions/node/v18.20.8/bin
 SCRIPT_PATH=/home1/irteam/apps/lunch-bot/dist
 
 $NODE_BIN_PATH/node $SCRIPT_PATH/script.bundle.js $@
